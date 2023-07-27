b9827b32b240e6875bc9bebe6e29d3e36a172c60
Usuario <britezxavierluca@gmail.com>
2023-07-27 16:05:26 -0300

    Eu, 2 commit en un dia, nice, pero ojo, por que tuviste que instalar una version de bootstrap rara

--- src/app/clientes/clientes.jason.ts
+++ src/app/clientes/clientes.jason.ts
@@ -2,8 +2,7 @@ import { Cliente } from './cliente';
 
 export const CLIENTES : Cliente[] = [
 {id: 1,nombre: 'Luca', apellido: 'Britez', email: 'britezlucaxavier@gmail.com', createAt : '2023/25/07'},
-{id: 2,nombre: 'Alumine', apellido: 'Britez', email: 'britezlucaxavier@gmail.com', createAt : '2023/25/07'},
-{id: 3,nombre: 'Paula', apellido: 'Freytes', email: 'paulafreytes@gmail.com', createAt : '2023/25/07'}
-
+{id: 2,nombre: 'Alumine', apellido: 'Britez', email: '', createAt : '2023/25/07'},
+{id: 3,nombre: 'Paula', apellido: 'Freytes', email: 'paulafreytes@gmail.com', createAt : '2023/25/07'},
+{id: 4,nombre: 'Romina', apellido: 'Pascaretta', email: 'rominapascaretta@gmail.com', createAt: '2023/27/07'}
 ]
-
